65c478aff1349e3af30a6fa169ccab23f7255840
accountrev <55156874+accrev@users.noreply.github.com>
2022-04-14 16:02:42 -0400

    Version 1.11 Changes

--- FunkyChartGUI.lua
+++ FunkyChartGUI.lua
@@ -6,7 +6,7 @@
      / __/ / /_/ / / / / ,< / /_/ / /___/ / / / /_/ / /  / /_  
     /_/    \__,_/_/ /_/_/|_|\__, /\____/_/ /_/\__,_/_/   \__/  
                            /____/
-    v1.1
+    v1.11
     Made with ♥ by accountrev          
 
     Thanks for downloading and using my script, if you're here to just use it once or plan to use it many times.
@@ -18,6 +18,18 @@
     !!! Please report any bugs/questions over on the Issues tab on GitHub, I will try to respond ASAP. !!!
     !!! Please report any bugs/questions over on the Issues tab on GitHub, I will try to respond ASAP. !!!
     !!! Please report any bugs/questions over on the Issues tab on GitHub, I will try to respond ASAP. !!!
+
+    [VERSION 1.11]
+
+    - Added more checks to minimize errors
+    - Save data now gets reset every time an update changes it (to reduce errors)
+    - Charts that were converted from an update in the past will now not play and requires re-converting (to reduce errors)
+    - Added a time offset for notes (positive number = later, negative numbers = earlier)
+    - Fixed a bug where refreshing the list won't actually refresh unless you re-execute
+    - Fixed some Krnl-exclusive and Synapse-exclusive bugs
+    - Your player will not move anymore when using WASD or arrow keys while playing
+    - More optimizations
+
 
     [VERSION 1.1]
 
@@ -70,7 +82,21 @@
 
 --]]
 
+for _,v in pairs(game.CoreGui:GetChildren()) do
+    if v:FindFirstChild("Main") then
+        for __,vv in pairs(v.Main:GetChildren()) do
+            if vv.Name == "MainCorner" then
+                v:Destroy()
+            end
+        end
+    end
+end
+
+
+saveDataVersion = "1"
+
 data = {
+
     chartData = {
         chartNotes = {},
         chartName = "None",
@@ -85,20 +111,41 @@ data = {
         timeOffset = 0,
         side = "Left",
         executor = "",
-        lastplayed = ""
+        lastplayed = "",
+        version = saveDataVersion
+    },
+
+    versions = {
+        acceptedVersions = {
+            "v1.11"
+        },
+        loadingVersion = ""
     }
 }
 
 chartList = {}
 
-local errorLagBool
-local chartLink
-local currentlyloadedtext
+local errorLagBool, chartLink, currentlyloadedtext, status, errorDesc
+
+local consoleEnabled = true
 
 local RS = game:GetService("RunService")
+local CAS = game:GetService("ContextActionService")
 
-function console(message)
-    rconsoleprint("[FunkyChart] " .. message .. "\n")
+function console(message, before)
+    if consoleEnabled then
+        before = before or ""
+        rconsoleprint(before .. "[FunkyChart] " .. message .. "\n")
+    end
+end
+
+function versionCheck(versionString)
+    for _,v in ipairs(data.versions.acceptedVersions) do
+        if versionString == v then
+            return true
+        end
+    end
+    return false
 end
 
 function loadSetup()
@@ -165,41 +212,47 @@ function Announce(messagetitle, messagebody, duration, type)
 	sound:Destroy()
 end
 
+Announce("Script Loaded", "Welcome to FunkyChart!", 10, "main")
+
 function errorHandler(errorMessage)
-    Announce("An Oopsie Occurred!", errorMessage .. "\nPlease report this!", 100, "error")
-    console("An Oopsie Occurred!", errorMessage .. "\nPlease report this!")
+    Announce("A critical error occured!", errorMessage .. "\nPlease report this!", 100, "error")
+    console("[CRITICAL ERROR] " .. errorMessage .. "\nPlease report this!", "\n\n\n")
 end
 
 function loadChart(chart, silent)
-    
-    if chart == nil then
-        console("No chart was selected, ignoring...")
-        Announce("No chart selected", "Select a chart from the list.", 10, "error")
-        return
-    end
-
-    console("Loading chart " .. chart .. ", silent = " .. tostring(silent))
-
+    chart = chartLink or data.options.lastplayed
     silent = silent or false
 
-    if data.options.executor == "" then
-        console("Executor missing")
-        Announce("No executor selected", "Select an executor to load a song.", 10, "error")
-        return
-    end
+    console("[LOADING SONG " .. chart .. " WITH silent = " .. tostring(silent) .. "]", "\n\n")
+    console("Checking if chart is nil (meaning if a chart is selected)")
 
-    if not isfile(chart) then
-        console("Error", chart .. " does not exist!")
+    if chart == nil then
+        console("ERROR - NO CHART WAS SELECTED, ignoring request...")
+        Announce("No chart selected", "Select a chart from the list.", 10, "error")
+        return
+    elseif not isfile(chart) then
+        console("ERROR - " .. chart .. " does not exist in FunkyChart/Charts!")
         Announce("Error", chart .. " does not exist!", 10, "error")
         return
     else
-        data.options.lastplayed = chart
-        console("Recognized chart, now loadstring")
+        console("CHART WAS FOUND! Now reading chart...")
         loadstring(readfile(chart))()
     end
 
-    if not isfile(data.chartData.loadedAudioID) then
-        console("No Audio Found!", data.chartData.loadedAudioID .. " cannot be found for " .. chart .. ".")
+    console("CHART READ! Now validating chart...")
+
+    if not versionCheck(data.versions.loadingVersion) then
+        console("ERROR - " .. chart .. " was created using an unaccepted version! Version used: " .. data.versions.loadingVersion)
+        Announce("Invalid Version of Chart", chart .. " must be reconverted to a newer version of FunkyChart.", 10, "error")
+        resetData("customChart")
+        return
+    elseif data.options.executor == "" then
+        console("ERROR - Executor was not selected. Ignoring request...")
+        Announce("No executor selected", "Select an executor to load a song.", 10, "error")
+        resetData("customChart")
+        return
+    elseif not isfile(data.chartData.loadedAudioID) then
+        console("ERROR - No Audio Found! " .. data.chartData.loadedAudioID .. " cannot be found for " .. chart .. ".")
         Announce("No Audio Found!", data.chartData.loadedAudioID .. " cannot be found for " .. chart .. ".", 10, "error")
         resetData("customChart")
         return
@@ -213,12 +266,14 @@ function loadChart(chart, silent)
         end
 
         game.SoundService.ClientMusic.SoundId = data.chartData.loadedAudioID
+        data.options.lastplayed = chart
+
         if not silent then
             Announce("Song Loaded", data.chartData.chartName .. " - " .. data.chartData.chartAuthor, 10, "loaded")
             Data("s")
         end
 
-        console("Chart successfully loaded")
+        console("CHART SUCESSFULLY LOADED!")
     end
 end
 
@@ -244,30 +299,40 @@ function Data(mode)
             end
             console("Data saved.")
         elseif mode == "l" then
-            console("Loading data...")
+            console("Checking if data exists...")
             if (readfile and isfile and isfile(datafilename)) then
+                console("FILE EXISTS! Now checking if save data is up to date...")
                 data.options = game:GetService("HttpService"):JSONDecode(readfile(datafilename))
-                Announce("Save Data Loaded", "Welcome back " .. game.Players.LocalPlayer.Name .. "!", 10, "main")
-                loadChart(data.options.lastplayed, true)
-                Announce("Loaded Last Played Chart", data.chartData.chartName .. " - " .. data.chartData.chartAuthor, 10, "loaded")
-                console("Data and chart loaded.")
                 
-                for _,v in pairs(data.chartData) do
-                    console(tostring(v))
-                end
+                if data.options.version ~= saveDataVersion then
+                    console("Save data is not up to date and can cause problems. Forcing reset...")
+                    Announce("Old Save Data", "Your save data was not up to date. Resetting.", 1, "error")
+                    Data("r")
+                    return
+                else
+                    console("Save data is up to date.")
+                    Announce("Save Data Loaded", "Welcome back " .. game.Players.LocalPlayer.Name .. "!", 10, "main")
+                    loadChart(data.options.lastplayed, true)
+                    Announce("Loaded Last Played Chart", data.chartData.chartName .. " - " .. data.chartData.chartAuthor, 10, "loaded")
+                    console("Data and chart loaded.")
+                    
+                    for _,v in pairs(data.chartData) do
+                        console(tostring(v))
+                    end
 
-                for _,v in pairs(data.options) do
-                    console(tostring(v))
+                    for _,v in pairs(data.options) do
+                        console(tostring(v))
+                    end
                 end
-
             else
-                console("Could not find current data. New user?")
+                console("Could not find current data. New user or reset?")
                 Announce("First Time?", "Looks like you don't have any save data. Load a song to start!", 10, "main")
             end
-        elseif mode == "w" then
+        elseif mode == "r" then
             console("Deleting current data...")
             Announce("Deleting", "Deleting save data...", 10, "main")
             delfile(datafilename)
+            loadstring(game:HttpGet(('https://raw.githubusercontent.com/accountrev/funkychart/master/FunkyChartGUI.lua')))()
         end
     end
 end
@@ -277,10 +342,10 @@ function resetData(choice)
     if choice == "customChart" then
         data.chartData = {
             chartNotes = {},
-            chartName = "",
+            chartName = "None",
             chartNameColor = "<font color='rgb(255, 255, 255)'>%s</font>",
-            chartAuthor = "",
-            chartDifficulty = "",
+            chartAuthor = "None",
+            chartDifficulty = "None",
             chartConverter = game.Players.LocalPlayer.Name,
             loadedAudioID = ""
         }
@@ -291,6 +356,9 @@ function resetData(choice)
             executor = "",
             lastplayed = ""
         }
+    elseif choice == "sideandoffset" then
+        data.options.timeOffset = 0
+        data.options.side = "Left"
     end
 end
 
@@ -298,9 +366,9 @@ end
 function Chart(preventErrorLag)
     preventErrorLag = preventErrorLag or false
 
-    console("Playing a chart with preventerrorlag " .. tostring(preventErrorLag))
+    console("[NOW PLAYING CHART - " .. data.options.lastplayed .. "WITH preventErrorLag = " .. tostring(preventErrorLag) .. "]", "\n\n")
     if data.chartData.loadedAudioID == "" then
-        console("No audio found")
+        console("No chart has been loaded!")
         Announce("Load a song", "Load a song first you dummy!", 10, "error")
         return
     else
@@ -310,8 +378,15 @@ function Chart(preventErrorLag)
                 Announce("Now Loading", data.chartData.chartName .. " - " .. data.chartData.chartAuthor, 1, "loaded")
 
                 if preventErrorLag then
+                    stagesArray = {}
+
+                    for _,v in pairs(game:GetService("Workspace").Map.Stages:GetChildren()) do
+                        table.insert(stagesArray, v)
+                    end
+
+                    local stagepos = stagesArray[math.random(#stagesArray)].Zone.CFrame
                     local currentpos = game.Players.LocalPlayer.Character.HumanoidRootPart
-                    local stagepos = game:GetService("Workspace").Map.Stages.WoodStage.Zone.CFrame
+                    
                     currentpos.CFrame = stagepos
                 end
                 
@@ -331,6 +406,8 @@ function Chart(preventErrorLag)
                 funky.SongPlayer:Countdown()
                 funky.SongPlayer.CurrentlyPlaying.Playing = true
 
+                game.Players.LocalPlayer.Character.Torso.Anchored = true
+
                 console("Playing successfully, now waiting for finish")
                 
                 repeat
@@ -343,6 +420,7 @@ function Chart(preventErrorLag)
 
                 game.SoundService.ClientMusic.Playing = false
                 game.SoundService.ClientMusic.TimePosition = 0
+                game.Players.LocalPlayer.Character.Torso.Anchored = false
 
                 console("Song done")
             end 
@@ -352,14 +430,12 @@ end
 
 
 function loadGUI()
-    chartList = listfiles("FunkyChart/Charts/")
-
     local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/accountrev/gui-libraries/main/kavo.lua')))()
 
-    local Window = Library.CreateLib("FunkyChart - v1.1", "GrapeTheme")
+    local Window = Library.CreateLib("FunkyChart - v1.11", "GrapeTheme")
 
     local Main = Window:NewTab("Main")
-    local CurrentyLoadedSec = Main:NewSection("Currently Loaded: None")
+    local CurrentyLoadedSec = Main:NewSection("Currently Loaded: None - None")
 
     local ChartLoading = Window:NewTab("Chart Loading")
     local ChartSec = ChartLoading:NewSection("Chart Loading")
@@ -374,7 +450,11 @@ function loadGUI()
     local Issues = Window:NewTab("Issues?")
 
     local PlayChartButton = CurrentyLoadedSec:NewButton("Play Chart", "Plays the chart you converted.", function()
-        Chart(errorLagBool)
+        status, errorDesc = pcall(Chart, errorLagBool)
+        if not status then
+            errorHandler(errorDesc)
+            game.Players.LocalPlayer.Character.Torso.Anchored = false
+        end
     end)
 
     local AutoplayerButton = CurrentyLoadedSec:NewButton("wally-rblx's AutoPlayer", "https://github.com/wally-rblx/funky-friday-autoplay", function()
@@ -390,11 +470,14 @@ function loadGUI()
     end)
 
     local refreshChartButton = ChartSec:NewButton("Refresh Charts", "Refreshes the chart list.", function()
-        ChartDropdown:Refresh(chartList)
+        ChartDropdown:Refresh(listfiles("FunkyChart/Charts/"))
     end)
 
     local LoadChartButton = ChartSec:NewButton("Load Chart", "Loads chart into your save data.", function()
-        loadChart(chartLink, false)
+        status, errorDesc = pcall(loadChart, chartLink)
+        if not status then
+            errorHandler(errorDesc)
+        end
     end)
 
     local PreventErrorLagToggle = GeneralSec:NewToggle("Prevent Error Lag", "Prevents massive lagspikes from errors.", function(state)
@@ -402,11 +485,38 @@ function loadGUI()
     end)
 
     local PlayerSideDropdown = GeneralSec:NewDropdown("Player Side", "Select between left side and right side.", {"Left", "Right"}, function(selectedOpt)
-        data.options.side = mob
-        Data("s")
+        data.options.side = selectedOpt
     end)
 
-    local TitleSizeSlide = GUISec:NewSlider("Title Size", "Adjust the size of the title while in-game.", 500, 0, function(s)
+    local TimeOffsetSlide = GeneralSec:NewSlider("Time Offset", "Milliseconds. Postive = Late, Negative = Early", 500, -500, function(s)
+        data.options.timeOffset = s / 1000
+        print(data.options.timeOffset, s)
+    end)
+
+    local ApplyGeneralButton = GeneralSec:NewButton("Apply Changes", "Applies your player side and offset changes.", function()
+        status, errorDesc = pcall(function()
+            loadChart(chartLink, true)
+        end)
+        if not status then
+            errorHandler(errorDesc)
+        end
+        console("Changes have been applied with time offset of " .. tostring(data.options.timeOffset) .. "and player side of " .. tostring(data.options.side))
+        Announce("Changes Applied", "Time Offset: " .. tostring(data.options.timeOffset) .. "\nPlayer Side: " .. tostring(data.options.side), 10, "loaded")
+    end)
+
+    local ResetGeneralButton = GeneralSec:NewButton("Reset to Default", "Resets your player side and offset to default.", function()
+        status, errorDesc = pcall(function()
+            resetData("sideandoffset")
+            loadChart(chartLink, true)
+        end)
+        if not status then
+            errorHandler(errorDesc)
+        end
+        console("Changes have been applied with time offset of " .. tostring(data.options.timeOffset) .. "and player side of " .. tostring(data.options.side))
+        Announce("Changes Applied", "Time Offset: " .. tostring(data.options.timeOffset) .. "\nPlayer Side: " .. tostring(data.options.side), 10, "loaded")
+    end)
+
+    local TitleSizeSlide = GUISec:NewSlider("Title Size", "Adjusts the size of the title while in-game.", 500, 0, function(s)
         game.Players.LocalPlayer.PlayerGui.GameUI.TopbarLabel.Size = UDim2.new(0.4, 0, 0, s)
     end)
 
@@ -421,14 +531,11 @@ function loadGUI()
             game:GetService("ReplicatedStorage").Assets.UI.Templates.Hits.Sick.Image = "rbxassetid://6450258128"
         end
     end)
-    --[[
     local ConsoleToggle = GUISec:NewToggle("Enable Debug Console", "Opens a console with more info.", function(state)
         consoleEnabled = state
     end)
-    --]]
     local DeleteSaveButton = OtherSec:NewButton("DELETE SAVE", "Use this as a last resort.", function()
         Data("w")
-        loadstring(game:HttpGet(('https://raw.githubusercontent.com/accountrev/funkychart/master/FunkyChartGUI.lua')))()
     end)
 
     local Credits1 = Credits:NewSection("Credits to:")
@@ -456,7 +563,7 @@ function loadGUI()
     end)
 
     
-    ChartDropdown:Refresh(chartList)
+    ChartDropdown:Refresh(listfiles("FunkyChart/Charts/"))
 
     RS.Heartbeat:Connect(function()
         CurrentyLoadedSec:UpdateSection("Currently Loaded: " .. data.chartData.chartName .. " - " .. data.chartData.chartAuthor)
@@ -469,11 +576,6 @@ function Init()
     Data("l")
 
     loadGUI()
-    Announce("Script Loaded", "Welcome to FunkyChart!", 10, "main")
 end
-
-local errorLagBool
-local chartListName
-local chartLink
 
 Init()
